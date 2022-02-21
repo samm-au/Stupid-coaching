b86fbee26307df5ce0b5f733cf165f2aeb1ccf0d
Samantha Au <samanthaau@live.com>
2022-02-21 15:13:34 +1100

    Created stupid coaching page

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+  def questions
+  end
+
+  def answer
+    @answer = if params[:answer.downcase] == 'i am going to work right now!'
+                'Great'
+              elsif params[:answer.downcase].end_with?('?')
+                'Silly question, get dressed and go to work!'
+              else
+                'I dont care, get dressed and go to work!'
+              end
+  end
+end
